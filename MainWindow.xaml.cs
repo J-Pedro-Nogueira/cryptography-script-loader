c8ea01b64f99ce6eaf5064b954fb57fcf6fde691
Pedro Nogueira <nogueirajosepedro@gmail.com>
2024-06-13 23:57:59 +0100

    project start

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Collections.Generic;
+using System.IO;
+using static System.Net.WebRequestMethods;
+
+namespace Cripto1
+{
+
+    public partial class MainWindow : Window
+    {
+
+        private string input_file_path;
+        private string key_file_path;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonQuit_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+                
+        }
+           
+
+        private void buttonDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonChooseKey_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonChooseTargetDirectory_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PopulateXlsFilesList(string directory)
+        {
+
+        }
+
+        private void buttonCreateKey_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonChooseDll_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void buttonChooseScriptPath_Click(object sender, RoutedEventArgs e)
+        {  
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # cryptography-script-loader
+
+Load modular cryptographic functions from a simple GUI.
+
+<img src="img\gameplan.svg">
+
+# Included scripts
+
+- <b>xls file encryption and decryption (proof of concept)</b>
+</br> Symmetric encryption with AES-128 CBC, storing nonce inside file.
+
+# Usage
+TODO
+
+<img src="img\cap1.png" width="600px">
